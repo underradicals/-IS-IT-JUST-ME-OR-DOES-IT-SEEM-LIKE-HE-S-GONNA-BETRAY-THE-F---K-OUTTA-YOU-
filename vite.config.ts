1b1338ae6149a7f583baf58986eac7ca04317e3f
Joseph <underradicals@gmail.com>
2025-05-19 04:57:22 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   .gitignore
    	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,60 @@
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { defineConfig } from 'vite';
-import type { UserConfig, ServerOptions, BuildEnvironmentOptions, UserConfigFnObject } from 'vite';
+import { readFileSync } from 'node:fs';
+import { createLogger, defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
+import type { UserConfig, ServerOptions, BuildEnvironmentOptions, UserConfigFnObject, JsonOptions } from 'vite';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const logger = createLogger('info', { allowClearScreen: true });
 
 type DevelopmentOptions = 'development' | 'production';
 const Environment: DevelopmentOptions = 'development';
 
-const Server = {} satisfies ServerOptions;
+const Server = {
+  port: 3000,
+  host: '0.0.0.0',
+  open: true,
+  https: {
+    key: readFileSync("./certs2/localhost.key"),
+    cert: readFileSync("./certs2/localhost.crt")
+  }
+} satisfies ServerOptions;
 
-const Build = {} satisfies BuildEnvironmentOptions;
+const Build = {
+  outDir: 'dist',
+  emptyOutDir: true
+} satisfies BuildEnvironmentOptions;
 
-const CSS = {} satisfies UserConfig["css"]
+const CSS = {
+  devSourcemap: true,
+  preprocessorOptions: {
+    scss: {
+      api: 'modern-compiler'
+    }
+  },
+  preprocessorMaxWorkers: 0,
+  postcss: {
+    plugins: [autoprefixer({ overrideBrowserslist: ["defaults and fully supports es6-module", "maintained node versions"] })]
+  },
+  modules: {
+    generateScopedName: "[name]__[local]___[hash:base64:5]",
+    exportGlobals: true,
+    localsConvention: 'camelCaseOnly'
+  }
+} satisfies UserConfig["css"]
+
+const JSON = {
+  namedExports: true
+} satisfies JsonOptions
 
 const ViteDevConfiguration = {
   server: Server,
   build: Build,
-  css: CSS
+  css: CSS,
+  json: JSON,
+  customLogger: logger
 } satisfies UserConfig;
 
 const ViteProdConfiguration = {
