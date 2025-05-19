2f3cba128e41932658a5e16794bf9af8b34fa6a5
Joseph <underradicals@gmail.com>
2025-05-19 01:59:03 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   package-lock.json
    	modified:   vite.config.ts
    diff --git c/vite.config.ts i/vite.config.ts
    index c00db35..58b472a 100644
    --- c/vite.config.ts
    +++ i/vite.config.ts
    @@ -1,5 +1,36 @@
    -import { dirname, resolve } from 'node:path'
    -import { fileURLToPath } from 'node:url'
    -import { defineConfig } from 'vite'
    +import { dirname, resolve } from 'node:path';
    +import { fileURLToPath } from 'node:url';
    +import { defineConfig } from 'vite';
    +import type { UserConfig, ServerOptions, BuildEnvironmentOptions, UserConfigFnObject } from 'vite';
    
    -const __dirname = dirname(fileURLToPath(import.meta.url))
    \ No newline at end of file
    +const __dirname = dirname(fileURLToPath(import.meta.url));
    +
    +type DevelopmentOptions = 'development' | 'production';
    +const Environment: DevelopmentOptions = 'development';
    +
    +const Server = {} satisfies ServerOptions;
    +
    +const Build = {} satisfies BuildEnvironmentOptions;
    +
    +const CSS = {} satisfies UserConfig["css"]
    +
    +const ViteDevConfiguration = {
    +  server: Server,
    +  build: Build,
    +  css: CSS
    +} satisfies UserConfig;
    +
    +const ViteProdConfiguration = {
    +
    +} satisfies UserConfig;
    +
    +
    +const ViteConfigurationFunction = (({ command, mode, isSsrBuild, isPreview }) =>
    +(command === 'serve'
    +  ? ViteDevConfiguration
    +  : ViteProdConfiguration)) as UserConfigFnObject;
    +
    +
    +export default (Environment === 'development'
    +  ? defineConfig(ViteDevConfiguration)
    +  : defineConfig(ViteConfigurationFunction))

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,36 @@
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { defineConfig } from 'vite'
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import type { UserConfig, ServerOptions, BuildEnvironmentOptions, UserConfigFnObject } from 'vite';
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+type DevelopmentOptions = 'development' | 'production';
+const Environment: DevelopmentOptions = 'development';
+
+const Server = {} satisfies ServerOptions;
+
+const Build = {} satisfies BuildEnvironmentOptions;
+
+const CSS = {} satisfies UserConfig["css"]
+
+const ViteDevConfiguration = {
+  server: Server,
+  build: Build,
+  css: CSS
+} satisfies UserConfig;
+
+const ViteProdConfiguration = {
+
+} satisfies UserConfig;
+
+
+const ViteConfigurationFunction = (({ command, mode, isSsrBuild, isPreview }) =>
+(command === 'serve'
+  ? ViteDevConfiguration
+  : ViteProdConfiguration)) as UserConfigFnObject;
+
+
+export default (Environment === 'development'
+  ? defineConfig(ViteDevConfiguration)
+  : defineConfig(ViteConfigurationFunction))
