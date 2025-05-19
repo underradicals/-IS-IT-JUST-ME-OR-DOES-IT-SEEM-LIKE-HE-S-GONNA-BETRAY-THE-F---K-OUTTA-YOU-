8e739a5aba495cc125de62bf4c5b6a749fb31aed
Joseph <underradicals@gmail.com>
2025-05-19 01:32:24 -0500

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,5 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
